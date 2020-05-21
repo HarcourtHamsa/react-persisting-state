41b942d1bd9c9e0a31f4f5cf913850599f9fba9a
Harcourt Hamsa <hamsaharcourt@gmail.com>
2020-05-21 22:56:48 +0100

    fixing issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const dataFromLocalStorage = localStorage.getItem("is-logged-in")
+    if (dataFromLocalStorage) {
+      setIsLoggedIn(JSON.parse(dataFromLocalStorage))
+    }
+  }, [setIsLoggedIn])
+
+  
+  useEffect(() => {
+    localStorage.setItem("is-logged-in", JSON.stringify(isLoggedIn))
+  }, [isLoggedIn])
+
+
+
+  const handleClick = () => {
+    setIsLoggedIn(!isLoggedIn)
+  }
   return (
-    <div>
-      
+    <div className="center">
+      <p>Hey, you are currently {isLoggedIn ? "Logged in" : "Logged out"}</p>
+      <button className="btn" onClick={handleClick}>
+        {isLoggedIn ? "Please log out" : "Please log in"}
+      </button>
     </div>
   )
 }
